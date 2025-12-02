4803402e2af31d744e9c469e7773ed1d21cf2474
akhwan <akhwan.dev@gmail.com>
2025-12-02 22:22:46 +0700

    remove cached pdf and config.js

--- fiveserver.config.js
+++
@@ -1,6 +0,0 @@
-// fiveserver.config.js
-module.exports = {
-  // Sesuaikan dengan jalur PHP di sistem operasi Anda
-  php: "C:\\xampp\\php\\php.exe" // Contoh untuk Windows (XAMPP)
-  // php: "/usr/bin/php" // Contoh untuk macOS/Ubuntu
-}
